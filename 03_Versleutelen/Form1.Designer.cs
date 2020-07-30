6a76bdeed798df35dbfabdbbd547aba240a71a74
youri <a;sldkfjfjdksla>
2020-07-30 13:21:36 +0200

    initial proof of concept. todo: stringinput.each -> foreach stringinput[i] that equals to cipher.with-value-of[i], possibilities-array[i].add. then print all possible combinations

---
+++ 03_Versleutelen/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace _03_Versleutelen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btn_to = new System.Windows.Forms.Button();
+            this.btn_from = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(603, 266);
+            this.textBox.TabIndex = 0;
+            // 
+            // btn_to
+            // 
+            this.btn_to.Location = new System.Drawing.Point(12, 284);
+            this.btn_to.Name = "btn_to";
+            this.btn_to.Size = new System.Drawing.Size(163, 63);
+            this.btn_to.TabIndex = 2;
+            this.btn_to.Text = "btn_to";
+            this.btn_to.UseVisualStyleBackColor = true;
+            this.btn_to.Click += new System.EventHandler(this.btn_to_Click);
+            // 
+            // btn_from
+            // 
+            this.btn_from.Location = new System.Drawing.Point(452, 284);
+            this.btn_from.Name = "btn_from";
+            this.btn_from.Size = new System.Drawing.Size(163, 63);
+            this.btn_from.TabIndex = 3;
+            this.btn_from.Text = "btn_from";
+            this.btn_from.UseVisualStyleBackColor = true;
+            this.btn_from.Click += new System.EventHandler(this.btn_from_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 359);
+            this.Controls.Add(this.btn_from);
+            this.Controls.Add(this.btn_to);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btn_to;
+        private System.Windows.Forms.Button btn_from;
+    }
+}
+
---
+++ 03_Versleutelen/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _03_Versleutelen
+{
+    public partial class Form1 : Form
+    {
+        static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        static uint[] VALUES = { 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1 };
+        
+        string thecipher = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox.Text = septenary("abcdefghijklmnopqrstuvwxyz");
+        }
+
+        private string septenary(string input)
+        {
+            var dictionary = new Dictionary<char, uint>();
+            uint i = 0;
+            do { dictionary.Add(ALPHABET[i], VALUES[i % 13]); i++; } while (i < ALPHABET.Length);
+
+            foreach (var key in dictionary)
+            {
+                thecipher += key.Value.ToString();
+            }
+            Console.WriteLine(thecipher);
+
+            string output = "";
+            char[] cinput = input.ToCharArray();
+
+            foreach (char ch in cinput)
+            {
+                int it = (int)ch;
+                if (it > 96 && i < 123)
+                {
+                    output += dictionary[ch];
+                }
+                else if (ch == ' ')
+                {
+                    output += ' ';
+                }
+            }
+            Console.WriteLine(output);
+            return output;
+        }
+        
+        private string deseptenary(string str, int key = 0)
+        {
+            string output = "";
+
+            var dictionary = new Dictionary<char, uint>();
+            uint i = 0;
+            do { dictionary.Add(ALPHABET[i], VALUES[i % 13]); i++; } while (i < ALPHABET.Length);
+
+            //i = 0;
+            //var dictionary2 = new Dictionary<uint, char>();
+            //do { dictionary2.Add(VALUES[i % 13], ALPHABET[i]); i++; } while (i < ALPHABET.Length);
+
+            /*
+            new char[][] allLetters = { [] }
+            
+            foreach (char charIndex in str)
+                str
+            allLetters.Append(new char[] { });    
+            
+
+            */
+            string[] possibilities = {};
+            // foreach 'int'-value ch in dictionary2.keys
+            uint temp = 0;
+
+            foreach (char ch in str)
+            {
+                //foreach (dictionary2.TryGetValue(possibilities[ch]) in )
+                //foreach (char ch2 in dictionary2.TryGetValue())
+                dictionary.TryGetValue(ch, out temp);
+                output += "char: " + ch + "\ttemp: " + temp + "\n";
+            }
+            return output;
+        }
+
+        private void btn_to_Click(object sender, EventArgs e)
+        {
+            textBox.Text = septenary(textBox.Text.ToLower());
+        }
+
+        private void btn_from_Click(object sender, EventArgs e)
+        {
+            textBox.Text = deseptenary(textBox.Text.ToLower());
+        }
+    }
+}
