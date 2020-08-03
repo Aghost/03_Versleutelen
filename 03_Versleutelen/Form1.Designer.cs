de55b0c07ab3142e53bfce9ba87e42b503f686bc
youri <a;sldkfjfjdksla>
2020-08-03 23:11:46 +0200

    todo: save to text

--- 03_Versleutelen/Form1.Designer.cs
+++ 03_Versleutelen/Form1.Designer.cs
@@ -31,46 +31,117 @@
             this.textBox = new System.Windows.Forms.TextBox();
             this.btn_to = new System.Windows.Forms.Button();
             this.btn_from = new System.Windows.Forms.Button();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.keyBox = new System.Windows.Forms.CheckBox();
+            this.extraBox = new System.Windows.Forms.CheckBox();
+            this.rst_Btn = new System.Windows.Forms.Button();
+            this.btn_revfrom = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox
             // 
+            this.textBox.AcceptsReturn = true;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox.Location = new System.Drawing.Point(12, 12);
             this.textBox.Multiline = true;
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(603, 266);
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(652, 243);
             this.textBox.TabIndex = 0;
             // 
             // btn_to
             // 
+            this.btn_to.BackColor = System.Drawing.SystemColors.ControlLight;
             this.btn_to.Location = new System.Drawing.Point(12, 284);
             this.btn_to.Name = "btn_to";
-            this.btn_to.Size = new System.Drawing.Size(163, 63);
+            this.btn_to.Size = new System.Drawing.Size(81, 65);
             this.btn_to.TabIndex = 2;
-            this.btn_to.Text = "btn_to";
-            this.btn_to.UseVisualStyleBackColor = true;
+            this.btn_to.Text = "Encode";
+            this.btn_to.UseVisualStyleBackColor = false;
             this.btn_to.Click += new System.EventHandler(this.btn_to_Click);
             // 
             // btn_from
             // 
-            this.btn_from.Location = new System.Drawing.Point(452, 284);
+            this.btn_from.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_from.Location = new System.Drawing.Point(595, 284);
             this.btn_from.Name = "btn_from";
-            this.btn_from.Size = new System.Drawing.Size(163, 63);
+            this.btn_from.Size = new System.Drawing.Size(69, 65);
             this.btn_from.TabIndex = 3;
-            this.btn_from.Text = "btn_from";
-            this.btn_from.UseVisualStyleBackColor = true;
+            this.btn_from.Text = "Decode";
+            this.btn_from.UseVisualStyleBackColor = false;
             this.btn_from.Click += new System.EventHandler(this.btn_from_Click);
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.keyTextBox.Location = new System.Drawing.Point(99, 285);
+            this.keyTextBox.Multiline = true;
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.keyTextBox.Size = new System.Drawing.Size(340, 64);
+            this.keyTextBox.TabIndex = 4;
+            // 
+            // keyBox
+            // 
+            this.keyBox.AutoSize = true;
+            this.keyBox.Checked = true;
+            this.keyBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.keyBox.Location = new System.Drawing.Point(99, 261);
+            this.keyBox.Name = "keyBox";
+            this.keyBox.Size = new System.Drawing.Size(88, 17);
+            this.keyBox.TabIndex = 5;
+            this.keyBox.Text = "generate key";
+            this.keyBox.UseVisualStyleBackColor = true;
+            // 
+            // extraBox
+            // 
+            this.extraBox.AutoSize = true;
+            this.extraBox.Checked = true;
+            this.extraBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.extraBox.Location = new System.Drawing.Point(333, 261);
+            this.extraBox.Name = "extraBox";
+            this.extraBox.Size = new System.Drawing.Size(106, 17);
+            this.extraBox.TabIndex = 6;
+            this.extraBox.Text = "show extra chars";
+            this.extraBox.UseVisualStyleBackColor = true;
+            // 
+            // rst_Btn
+            // 
+            this.rst_Btn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.rst_Btn.Location = new System.Drawing.Point(445, 284);
+            this.rst_Btn.Name = "rst_Btn";
+            this.rst_Btn.Size = new System.Drawing.Size(69, 66);
+            this.rst_Btn.TabIndex = 7;
+            this.rst_Btn.Text = "Reset";
+            this.rst_Btn.UseVisualStyleBackColor = false;
+            this.rst_Btn.Click += new System.EventHandler(this.rst_Btn_Click);
+            // 
+            // btn_revfrom
+            // 
+            this.btn_revfrom.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_revfrom.Location = new System.Drawing.Point(520, 284);
+            this.btn_revfrom.Name = "btn_revfrom";
+            this.btn_revfrom.Size = new System.Drawing.Size(69, 66);
+            this.btn_revfrom.TabIndex = 8;
+            this.btn_revfrom.Text = "Reverse Alphabet Cipher";
+            this.btn_revfrom.UseVisualStyleBackColor = false;
+            this.btn_revfrom.Click += new System.EventHandler(this.btn_revfrom_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 359);
+            this.ClientSize = new System.Drawing.Size(670, 355);
+            this.Controls.Add(this.btn_revfrom);
+            this.Controls.Add(this.rst_Btn);
+            this.Controls.Add(this.extraBox);
+            this.Controls.Add(this.keyBox);
+            this.Controls.Add(this.keyTextBox);
             this.Controls.Add(this.btn_from);
             this.Controls.Add(this.btn_to);
             this.Controls.Add(this.textBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Septenary";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -82,6 +153,11 @@
         private System.Windows.Forms.TextBox textBox;
         private System.Windows.Forms.Button btn_to;
         private System.Windows.Forms.Button btn_from;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.CheckBox keyBox;
+        private System.Windows.Forms.CheckBox extraBox;
+        private System.Windows.Forms.Button rst_Btn;
+        private System.Windows.Forms.Button btn_revfrom;
     }
 }
 
--- 03_Versleutelen/Form1.cs
+++ 03_Versleutelen/Form1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security;
 using System.Windows.Forms;
 
 namespace _03_Versleutelen
@@ -7,7 +11,12 @@ namespace _03_Versleutelen
     public partial class Form1 : Form
     {
         static char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        static char[] TEBAHPLA = "abcdefghijklmnopqrstuvwxyz".Reverse().ToArray();
+        bool flippedAlpha = false;
+
         static byte[] VALUES = { 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1 };
+
+        string keystring = "";
         
         Dictionary<char, byte> dictionary =
             new Dictionary<char, byte>();
@@ -20,36 +29,39 @@ namespace _03_Versleutelen
         private void Form1_Load(object sender, EventArgs e)
         {
             byte i = 0;
-            do { dictionary.Add(ALPHABET[i], VALUES[i % 13]); i++; } while (i < ALPHABET.Length);
+            do { dictionary.Add(ALPHABET[i], VALUES[i % VALUES.Length]); i++; } while (i < ALPHABET.Length);
 
-            // textBox.Text = "As the moon changes her phases every seven days; this number influences all sublunary beings. -Shakespeare";
-            // textBox.Text = "By its occult virtues tended to the accomplishment of all things, to be the dispenser of life and fountain of all its changes. -Hippocrates";
-            textBox.Text = "With the child, it is the teeth that appear in the seventh month and he sheds them at seven years; at twice seven puberty begins, at three times seven all our mental and vital powers are developed, at four times seven he is in his full strength, at five times seven his passions are most developed";
+            textBox.Text = "With the child, it is the teeth that appear in the seventh month and he sheds them at seven years; at twice seven puberty begins, at three times seven all our mental and vital powers are developed, at four times seven he is in his full strength, at five times seven his passions are most developed -Hippocrates";
         }
 
         private string septenary(string input)
         {         
             string output = "";
-            char[] cinput = input.ToCharArray();
+            char[] cinput = input.ToLower().ToCharArray();
 
+            char[] keyValues = "11111112222223333332444444".ToCharArray();
+            keystring = "";
+
+            int x = 0;
             foreach (char ch in cinput)
             {
-                byte i = (byte)ch;
-
-                if (i > 96 && i < 123)
+                switch (ch)
                 {
-                    output += dictionary[ch];
+                    case char i when (ch > 96 && ch < 123):
+                        keystring += keyValues[ch - 97];
+                        output += dictionary[ch];
+                        break;
+                    case ' ': if (x != 0 && cinput[x - 1] != ' ') { keystring += '0'; output += '0'; }; break;
+                    case '.': goto default;
+                    case ',': goto default;
+                    case ';': goto default;
+                    case char i when (ch > 32 && ch < 48): goto default;
+                    default: if (extraBox.Checked == true) { keystring += ch; output += ch; break; } else keystring += '0'; output += '0'; break;
                 }
-                else if (ch == ' ')
-                {
-                    output += ' ';
-                }
-                else
-                {
-                    output += ' ';              // reserved for special characters
-                }
+                x++;
             }
-            Console.WriteLine(output);
+            if (keyBox.Checked == true)
+                keyTextBox.Text = keystring;
 
             return output;
         }
@@ -62,46 +74,83 @@ namespace _03_Versleutelen
             int x = 0;
             foreach (char chstr in input)
             {
-                if (chstr == ' ')
+                switch (chstr)
                 {
-                    parray[x] = new char[1];
-                    parray[x][0] = ' ';
+                    case '0': if (x != 0 && input[x -1] != '0') { output += ' '; }; break;
+                    case '.': goto default;
+                    case ',': goto default;
+                    case ';': goto default;
+                    case char i when (i > 48 && i < 56):
+                        if (i == 55) { parray[x] = new char[2]; }
+                        else            parray[x] = new char[4];
 
-                    Console.WriteLine($"add {parray[x][0]} parray at spot {x}:0");
-                }
-
-                else if (chstr > 48 && chstr < 56)
-                {
-                    byte arraysize = 4;
-                    if (chstr == 55) { arraysize -= 2 ; };
-                    parray[x] = new char[arraysize];
-
-                    int y = 0;
-                    foreach (char numberchar in dictionary.Keys)
-                    {
-                        if (dictionary[numberchar].ToString() == (chstr - 48).ToString())
+                        int y = 0;
+                        foreach (char numberchar in dictionary.Keys)
                         {
-                            parray[x][y] = numberchar;      // add to parray
-                            y++;
+                            if (dictionary[numberchar].ToString() == (chstr - 48).ToString())
+                            {
+                                parray[x][y] = numberchar;
+                                y++;
+                            }
                         }
-                    }
+                        
+                        if (keyTextBox.Text.Length != input.Length)
+                        {
+                            y = 0;
+                            output += $" |_ ";
+                            foreach (char value in parray[x]) { output += parray[x][y]; y++; };
+                            output += " _| ";
+                        }
+                        else
+                            try { output += parray[x][keyTextBox.Text[x] - 49];
+                            } catch { output += 'X'; }
+                        break;
+
+                    default: if (extraBox.Checked == true) { parray[x] = new char[1]; parray[x][0] = chstr; output += chstr; break; }  break;
                 }
-                Array.ForEach(parray[x], element => output += element.ToString());
                 x++;
             }
-            Console.WriteLine(output);
 
             return output;
         }
 
         private void btn_to_Click(object sender, EventArgs e)
         {
-            textBox.Text = septenary(textBox.Text.ToLower());
+            textBox.Text = septenary(textBox.Text);
         }
-
         private void btn_from_Click(object sender, EventArgs e)
         {
-            textBox.Text = deseptenary(textBox.Text.ToLower());
+            textBox.Text = deseptenary(textBox.Text);
+        }
+
+        private void btn_revfrom_Click(object sender, EventArgs e)
+        {
+            flippedAlpha = !flippedAlpha;
+                        
+            dictionary.Clear();
+
+            byte i = 0;
+            /*            
+            do { dictionary.Add(ALPHABET[tempsize], VALUES[i % VALUES.Length]); i++;
+            } while (i < ALPHABET.Length);
+            */
+            
+            if (flippedAlpha) {
+                do { dictionary.Add(ALPHABET[ALPHABET.Length - 1 - i], VALUES[i % VALUES.Length]); i++; }
+                while (i < TEBAHPLA.Length);
+                btn_revfrom.BackColor = Color.FromName("Lime");
+            } else {
+                do { dictionary.Add(ALPHABET[i], VALUES[i % VALUES.Length]); i++; }
+                while (i < ALPHABET.Length);
+                btn_revfrom.BackColor = Color.FromName("ControlLight");
+            };
+        }
+
+        private void rst_Btn_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "";
+            keyTextBox.Text = "";
+            keystring = "";
         }
     }
 }
